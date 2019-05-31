12f0a0f0b96803f97e162116ab674a38f4c20b91
Jake Dudum <jakedudum@gmail.com>
2019-05-30 17:31:39 -0700

    Added a vertical button list to questionPage Jumbotron in index.html and added a testing object to app.js along with basic countdown functionality and display

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,59 @@
 $(document).ready(function () {
 
+    var questions = {
+        question1: {
+            question: "pick red",
+            correct: "red",
+            wrong: "blue",
+            wrong1: "green",
+            wrong2: "white"
+        },
+        question2: {
+            question: "pick blue",
+            correct: "blue",
+            wrong: "red",
+            wrong1: "green",
+            wrong2: "white"
+        },
+        question3: {
+            question: "pick white",
+            correct: "white",
+            wrong: "blue",
+            wrong1: "green",
+            wrong2: "red"
+        },
+    }
+
+    var timerID;
+    var number = 10;
+
 
     $(".start").on('click', function () {
         $("#startPage").addClass("d-none");
         $("#questionPage").removeClass("d-none");
+        timerID = setInterval(countdown, 1000);
     });
 
+    function countdown() {
+
+        number--;
+  
+        $("#countdown").text(number);
+  
+        if (number === 0) {
+          stopTrivia();
+          setTimeout(showAnswer, 1000);
+        }
+      }
+
+
+
+    function startTrivia() {
+        timerID = setInterval(askQuestion, 3000);
+    }
+
+    function stopTrivia() {
+        clearInterval(timerID);
+    }
+    
 });
